46edcdc5b97579df3a729576696988d452d0ba5c
seungju <kingsky32@gmail.com>
2020-07-25 15:06:31 +0900

    add permissions notifications

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ApolloProvider } from "react-apollo-hooks";
+import * as Permissions from "expo-permissions";
+import { Notifications } from "expo";
 import client from "./apollo";
 import Chat from "./Chat";
 
 export default function App() {
+  const [notificationStatus, setStatus] = useState(false);
+  const ask = async () => {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    console.log(status);
+  };
+  useEffect(() => {
+    ask();
+  }, []);
   return (
     <ApolloProvider client={client}>
       <Chat />
